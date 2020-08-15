4619473d73364064e40a48d45fe2ce56f1ac887f
KingOfRaccoon <zvezdakov2015@yandex.ru>
2020-08-15 17:55:06 +0700

    hfch

---
+++ Assets/ggf.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ggf : MonoBehaviour
+{
+	public bool change = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (change)
+		{
+			SceneManager.LoadScene("SampleScene");
+			change = false;
+		}
+	}
+	void startChange()
+	{
+		change = true;
+	}
+	
+}
